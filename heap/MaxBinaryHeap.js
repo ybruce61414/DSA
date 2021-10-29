8cb6c724a90da5b08f932a83e337086003a6062d
Viola <ybruce61414@gmail.com>
2021-10-29 17:17:02 +0800

    implement max heap

--- heap/MaxBinaryHeap.js
+++ heap/MaxBinaryHeap.js
@@ -1,1 +1,83 @@
-console.log('----hello world')
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+    this.size = 0;
+  }
+
+  insert(val) {
+    if (val === undefined) return;
+    this.values.push(val);
+    this.size += 1;
+
+    this._bubbleUp();
+    return this.values;
+  }
+
+  extractMax() {
+    if (this.values.length === 0) return;
+    this._swap(0, this.values.length - 1);
+    let max = this.values.pop();
+
+    this.size -= 1;
+    this._trickleDown();
+
+    return max;
+  }
+
+  _bubbleUp() {
+    let currIdx = this.values.length - 1;
+    let parentIx;
+
+    while (currIdx > 0) {
+      parentIx = Math.floor((currIdx - 1) / 2);
+      if (this.values[parentIx] >= this.values[currIdx]) break;
+      this._swap(currIdx, parentIx);
+      currIdx = parentIx;
+    }
+  }
+
+  _trickleDown() {
+    let currIdx = 0;
+    let leftChildIdx, rightChildIdx, swapIdx;
+
+    while (true) {
+      leftChildIdx = currIdx * 2 + 1;
+      rightChildIdx = currIdx * 2 + 2;
+      swapIdx = null;
+
+      if (
+        leftChildIdx < this.size &&
+        this.values[leftChildIdx] > this.values[currIdx]
+      ) {
+        swapIdx = leftChildIdx;
+      }
+      if (
+        rightChildIdx < this.size &&
+        ((swapIdx && this.values[rightChildIdx] > this.values[leftChildIdx]) ||
+          (!swapIdx && this.values[rightChildIdx] > this.values[currIdx]))
+      ) {
+        swapIdx = rightChildIdx;
+      }
+
+      if (!swapIdx) break;
+      this._swap(swapIdx, currIdx);
+      currIdx = swapIdx;
+    }
+  }
+
+  _swap(idx1, idx2) {
+    [this.values[idx1], this.values[idx2]] = [
+      this.values[idx2],
+      this.values[idx1],
+    ];
+  }
+}
+
+let maxheap = new MaxBinaryHeap();
+
+const array = [17, 2, 36, 100, 7, 1, 19, 25, 3];
+for (let i of array) maxheap.insert(i);
+
+console.log(maxheap.values);
+maxheap.extractMax();
+console.log(maxheap.values);
