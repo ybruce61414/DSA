195a76eb227f0e9991e6715d6851e8a834f8f2a8
viola.chen <viola.chen@violachendeMacBook-Pro.local>
2021-11-18 01:02:53 +0800

    wip

--- sorting/linearithmic/quickSort.js
+++ sorting/linearithmic/quickSort.js
@@ -1,10 +1,13 @@
+function swap(arr, idx1, idx2) {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
+
 function partition(arr, start, end) {
   // Taking the last element as the pivot
   const pivotValue = arr[end];
   let pivotIndex = start;
   for (let i = start; i < end; i++) {
     if (arr[i] < pivotValue) {
-      // Swapping elements
       [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
       pivotIndex++;
     }
@@ -13,6 +16,20 @@ function partition(arr, start, end) {
   // Putting the pivot value in the middle
   [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
   return pivotIndex;
+}
+
+function partitionFirst(arr, start, end) {
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = 0; i <= end; i++) {
+    if (arr[i] < pivot) {
+      swapIndex += 1;
+      if (i !== swapIndex) swap(arr, i, swapIndex + 1);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
 }
 
 function quickSort1(arr, start, end) {
@@ -67,10 +84,13 @@ function quickSort3(array, left, right) {
 const array1 = [7, -2, 4, 1, 6, 5, 0, -4, 2];
 const array2 = [7, -2, 4, 1, 6, 5, 0, -4, 2];
 const array3 = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+const array4 = [11, 40, 40, 50, 43, 10, 30, 42, 20, 6, 19, 32, 20, 41, 23, 27];
 
 quickSort1(array1, 0, array1.length - 1);
 let re2 = quickSort2(array2);
 
-console.log("1----", array1);
-console.log("2----", re2);
-console.log("3----", quickSort3(array3));
+// console.log("1----", array1);
+// console.log("2----", re2);
+// console.log("3----", quickSort3(array3));
+
+console.log("1----", partitionFirst(array4, 0, array4.length - 1));
